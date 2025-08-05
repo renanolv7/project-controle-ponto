b2c8bd61504407cf420eaddbc08f59a63b083076
renanolv7 <reviniciusoliveira@gmail.com>
2025-08-05 01:41:50 -0300

    "refactor: controller mudado para retornar uma entity e status Created"

--- back_end_code/api-controle-ponto/src/main/java/com/estudos/api_controle_ponto/controllers/ControlePontoControllers.java
+++ back_end_code/api-controle-ponto/src/main/java/com/estudos/api_controle_ponto/controllers/ControlePontoControllers.java
@@ -1,6 +1,8 @@
 package com.estudos.api_controle_ponto.controllers;
 
+import com.estudos.api_controle_ponto.models.ControlePontoEntity;
 import com.estudos.api_controle_ponto.services.ControlePontoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,7 @@ import java.time.LocalTime;
 @RestController
 @RequestMapping("controle")
 public class ControlePontoControllers {
+    
     private ControlePontoService controlePontoService;
     public ControlePontoControllers(ControlePontoService controlePontoService) {
         this.controlePontoService = controlePontoService;
@@ -24,8 +27,8 @@ public class ControlePontoControllers {
     }
 
     @PostMapping("/bater-ponto")
-    public ResponseEntity<String> baterPonto(@RequestBody PontoRequest request){
-        String infos = controlePontoService.registrarPonto(request.matricula, request.hora, request.data);
-        return ResponseEntity.ok(infos);
+    public ResponseEntity<ControlePontoEntity> baterPonto(@RequestBody PontoRequest request){
+        ControlePontoEntity pontoSalvo = controlePontoService.registrarPonto(request.matricula, request.hora, request.data);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pontoSalvo);
     }
 }
